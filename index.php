e0161f7844d23dfc38ccc6913a17aca47ec6a6f0
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-01-18 14:39:02 +0100

    j

--- index.php
+++ index.php
@@ -4,33 +4,34 @@
 $a = 50;
 $b = 10;
 
-       >    {
+if ($a > $b){
     echo "Hello World!";
 }
 
 //Deuxieme ligne
-    {
+if ($a !== $b){
     echo "<br><br>Hello World2";
 }
 
 //Troisieme ligne
-($a==b) {
+if ($a===$b) {
     echo "<br><br>Oui";
 }
 
-{
+else{
     echo "<br><br>Non";
 }
 
 
 //Quatrieme ligne
 echo"<br><br>";
- ($a == $b) {
+if($a === $b) {
     echo "1";
-}  ($a > $b) {
+}
+elseif($a > $b) {
     echo "2";
 }
-{
+else{
     echo "3";
 }
 
@@ -38,10 +39,13 @@ echo"<br><br>";
 //Cinquieme ligne
 echo"<br><br>";
 
- ($color) {
-    "red":
-    echo "Hello";
-   "green":
-    echo "Welcome";
+$color = "red";
+switch($color) {
+    case "red":
+        echo "Hello";
+        break;
+   case "green":
+        echo "Welcome";
+        break;
 }
 
